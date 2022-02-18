18c390b0737feeeaf3241e1d27cf795933226b3f
Arnaud Le Blanc <arnaud.lb@gmail.com>
2022-02-18 10:22:33 +0100

    Add exec hook

---
+++ etc/phpenv.d/exec/xdebug.bash
@@ -0,0 +1,13 @@
+#!/bin/false
+#
+# Summary: phpenv composer plugin
+set -e
+[ -n "$PHPENV_DEBUG" ] && set -x
+
+if [ "$PHPENV_COMMAND" = "php" ]; then
+    if ! [ -z "$XDEBUG_MODE" ]; then
+        arg0="$1"
+        shift 1
+        set -- "$arg0" -dzend_extension=xdebug.so "$@"
+    fi
+fi
